7654b1801cd78efb2aae1ec8f4c4eac536117c3c
GiaRushing1122 <giarushing@gmail.com>
2019-11-24 14:54:41 -0600

    Arithmetic operators

---
+++ buildingblocks.js
@@ -0,0 +1,19 @@
+// Reviewing Building Blocks of Javascript
+
+// Values and Variables
+
+// Primitive Types of Data in JS
+
+/* NUMBERS- a primitive data type witihtn Javascript that allows us to utilize numerical values */
+
+// We can use arithemtic operators to compute various mathematical problems
+
+/* Arithmetic Operators */
+
+var add = 1+1;
+var subtract = 2-1;
+var multiply = 3*3;
+var divide = 400/45;
+var modulo = 27%2;
+
+
