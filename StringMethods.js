c78d27da23030104fc7e5fa10072797de569b544
GiaRushing1122 <giarushing@gmail.com>
2019-12-25 22:23:47 -0600

    adding to string methods

---
+++ StringMethods.js
@@ -0,0 +1,27 @@
+//String Methods
+    //toUpperCase
+let msg = "You Are Beautiful"
+
+let upperCase = msg.toUpperCase();
+
+upperCase
+
+    //toLowerCase
+let lowerCase = msg.toLowerCase();
+
+lowerCase
+
+    //trim
+let city = "         New Orleans      "
+city
+
+let trimmed = city.trim();
+
+trimmed
+
+// we can chain these methods
+
+
+let chained = city.trim().toUpperCase();
+
+chained
